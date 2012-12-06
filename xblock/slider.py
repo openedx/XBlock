fd28f3c5c92dc02a065e99851923a835a34db163
Carlos Andrés Rocha <rocha@edx.org>
2012-12-05 20:06:54 -0500

    Refactor slider JS code

--- xblock/slider.py
+++ xblock/slider.py
@@ -4,7 +4,6 @@ from webob import Response
 from xblock.core import XBlock, Scope, Int
 from xblock.widget import Widget
 
-
 class Slider(XBlock):
     min_value = Int(help="Minimum value", default=0, scope=Scope.content)
     max_value = Int(help="Maximum value", default=100, scope=Scope.content)
@@ -12,34 +11,12 @@ class Slider(XBlock):
 
     @XBlock.view('student_view')
     def render_student(self, context):
-        template = '<input type="range" min="{min}" max="{max}" value="{val}"/> <span> {val} </span>'
-        html = template.format(min=self.min_value,
-                               max=self.max_value,
-                               val=self.value)
+        html = SLIDER_TEMPLATE.format(min=self.min_value,
+                                      max=self.max_value,
+                                      val=self.value)
         widget = Widget(html)
-
         widget.add_css("input[type=range] { width=100px; }")
-        widget.add_javascript("""
-            function Slider(runtime, element) {
-              console.log('slider constructor');
-
-              handler_url = runtime.handler_url('update');
-              input = $(element).children('input[type="range"]');
-              display = $(element).children('span');
-
-              $(element).on('ajaxSend', function(elm, xhr, s) {
-                runtime.prep_xml_http_request(xhr);
-              });
-
-              input.on('change', function () {
-                display.html(this.value);
-              });
-
-              input.on('mouseup', function () {
-                $.post(handler_url, JSON.stringify({value: this.value}));
-              });
-            };
-            """);
+        widget.add_javascript(SLIDER_JS);
         widget.initialize_js('Slider')
 
         return widget
@@ -56,21 +33,12 @@ class ProgressSlider(Slider):
     def render_student(self, context):
         widget = super(ProgressSlider, self).render_student(context)
 
-        # TODO: [rocha]
-        # non-wrapped ccs will make this global, not what we want
+        # TODO: [rocha] non-wrapped ccs will make this global
+        #               not what we want
         #
         # widget.add_css("input[type=range] + span { color: red; }")
 
-        widget.add_javascript("""
-           // TODO: [rocha] assuming Slider is in the outer scope...
-
-           function ProgressSlider(runtime, element) {
-             console.log('progress-slider');
-
-             Slider.call(this, runtime, element)
-             $(element).css('color','red');
-           }
-           """)
+        widget.add_javascript(P_SLIDER_JS)
         widget.initialize_js('ProgressSlider')
         return widget
 
@@ -79,3 +47,58 @@ class ProgressSlider(Slider):
         response = super(ProgressSlider, self).handle_update(request)
         self.runtime.publish('progress', self.value)
         return response
+
+SLIDER_TEMPLATE = """
+<input type="range" min="{min}" max="{max}" value="{val}"/> <span> {val} </span>
+"""
+
+SLIDER_JS = """
+function Slider(runtime, element) {
+  if (!(this instanceof Slider)) {
+    return new Slider(runtime, element);
+  }
+
+  this.handler_url = runtime.handler_url('update');
+  this.input = $(element).children('input[type="range"]');
+  this.output = $(element).children('span');
+
+  var self = this;
+
+  $(element).on('ajaxSend', function(elm, xhr, s) {
+    runtime.prep_xml_http_request(xhr);
+  });
+
+  self.input.on('change', function () {
+    self.output.html(this.value);
+  });
+
+  self.input.on('mouseup', function () {
+    $.post(self.handler_url, JSON.stringify({value: this.value}));
+  });
+};
+
+Slider.prototype.submit = function() {
+  return this.input.val();
+};
+
+Slider.prototype.handle_submit = function(result) {
+};
+"""
+
+P_SLIDER_JS = """
+function ProgressSlider(runtime, element) {
+  if (!(this instanceof ProgressSlider)) {
+    return new ProgressSlider(runtime, element);
+  }
+
+  Slider.call(this, runtime, element);
+
+  $(element).css('color','red');
+
+  this.input.on('mouseup', function() {
+    console.log('updating progress');
+  });
+}
+
+ProgressSlider.prototype = Object.create(Slider.prototype);
+"""
