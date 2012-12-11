614d132922cad458de7c18420bd994e8429fefaa
Ned Batchelder <ned@nedbatchelder.com>
2012-12-10 21:32:21 -0500

    Refactor this into a self-contained js object.

--- debugger/static/js/runtime/1.js
+++ debugger/static/js/runtime/1.js
@@ -1,20 +1,53 @@
 // XBlock runtime implementation.
 
-// Constructor for a runtime object provided to an XBlock init function.
-function runtime_1(element, children) {
-    var child_map = {}
-    $.each(children, function(idx, child) {
-        child_map[child.name] = child
-    });
+var XBlock = (function () {
+
+    // Constructors for a runtime object provided to an XBlock init function.
+    // Indexed by version number.  Only 1 right now.
+    var runtime_constructors = {
+        1: function (element, children) {
+            var child_map = {}
+            $.each(children, function(idx, child) {
+                child_map[child.name] = child
+            });
+            return {
+                handler_url: function(handler_name) {
+                    var usage = $(element).data('usage');
+                    return "/handler/" + usage + "/" + handler_name + "?student=" + student_id;
+                },
+                children: children,
+                child_map: child_map
+            };
+        }
+    };
+
+    var initializeBlock = function (element) {
+        var children = initializeBlocks($(element));
+
+        var version = $(element).data('runtime-version');
+        if (version === undefined) {
+            return null;
+        }
+
+        var runtime = runtime_constructors[version](element, children);
+        var init_fn = window[$(element).data('init')];
+        var js_block = init_fn(runtime, element) || {};
+        js_block.element = element;
+        js_block.name = $(element).data('name');
+        return js_block;
+    };
+
+    var initializeBlocks = function (element) {
+        return $(element).immediateDescendents('.xblock').map(function(idx, elem) {
+            return initializeBlock(elem);
+        }).toArray();
+    };
+
     return {
-        handler_url: function(handler_name) {
-            var usage = $(element).data('usage');
-            return "/handler/" + usage + "/" + handler_name + "?student=" + student_id;
-        },
-        children: children,
-        child_map: child_map
-    }
-}
+        initializeBlocks: initializeBlocks
+    };
+}());
+
 
 $(function() {
     // Find all the children of an element that match the selector, but only
@@ -31,32 +64,10 @@ $(function() {
         });
     };
 
-    function initializeBlock(element) {
-        var children = initializeBlocks($(element));
-
-        var version = $(element).data('runtime-version');
-        if (version === undefined) {
-            return null;
-        }
-
-        var runtime = window['runtime_' + version](element, children);
-        var init_fn = window[$(element).data('init')];
-        var js_block = init_fn(runtime, element) || {};
-        js_block.element = element;
-        js_block.name = $(element).data('name');
-        return js_block;
-    }
-
-    function initializeBlocks(element) {
-        return $(element).immediateDescendents('.xblock').map(function(idx, elem) {
-            return initializeBlock(elem);
-        }).toArray();
-    }
-
     $('body').on('ajaxSend', function(elm, xhr, s) {
         // Pass along the Django-specific CSRF token.
         xhr.setRequestHeader('X-CSRFToken', $.cookie('csrftoken'));
     });
 
-    initializeBlocks($('body'));
+    XBlock.initializeBlocks($('body'));
 });
