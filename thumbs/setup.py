64f9c4a8548aaf5ce01633da421f62fbb5da3458
Ned Batchelder <ned@nedbatchelder.com>
2013-11-14 09:17:18 -0500

    Rearrange thumbs so it will install properly.
    With py_modules, we weren't able to install package_data, so make this
    into a genuine Python package.

--- thumbs/setup.py
+++ thumbs/setup.py
@@ -1,19 +1,33 @@
-"""Set up for XBlock thumbs module"""
+"""Set up for XBlock thumbs block."""
 
+import os
 from setuptools import setup
+
+
+def package_data(pkg, root):
+    """Generic function to find package_data for `pkg` under `root`."""
+    data = []
+    for dirname, _, files in os.walk(os.path.join(pkg, root)):
+        for fname in files:
+            data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
 
 setup(
     name='thumbs-xblock',
     version='0.1',
     description='Thumbs XBlock Sample',
-    py_modules=['thumbs'],
-    install_requires=['XBlock'],
+    packages=[
+        'thumbs',
+    ],
+    install_requires=[
+        'XBlock',
+    ],
     entry_points={
         'xblock.v1': [
             'thumbs = thumbs:ThumbsBlock',
         ]
     },
-    package_data={
-        'thumbs': ['static/*'],
-    },
+    package_data=package_data("thumbs", "static"),
 )
