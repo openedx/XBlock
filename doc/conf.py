aad46f8f5717145a53d6b6b69a0ce2331b056ddb
Ned Batchelder <ned@edx.org>
2013-12-08 14:11:16 -0500

    Use the Napoleon sphinx plugin to allow nicer docstrings.

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,13 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.todo']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.todo',
+    'sphinxcontrib.napoleon',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
