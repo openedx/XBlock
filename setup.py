ea7351fc2fbc48c87337b2f46c49477273f40648
jorr <jorr@google.com>
2014-01-09 16:58:12 -0800

    Move the demo xblocks into their own package so that the xblock package can be installed without the demo xblocks.

--- setup.py
+++ setup.py
@@ -8,20 +8,5 @@ setup(
     packages=['xblock'],
     install_requires=[
         'webob',
-    ],
-    entry_points={
-        'xblock.v1': [
-            'helloworld_demo = xblock.content:HelloWorldBlock',
-            'html_demo = xblock.content:HtmlBlock',
-            'sequence_demo = xblock.structure:Sequence',
-            'vertical_demo = xblock.structure:VerticalBlock',
-            'sidebar_demo = xblock.structure:SidebarBlock',
-            'problem_demo = xblock.problem:ProblemBlock',
-            'textinput_demo = xblock.problem:TextInputBlock',
-            'equality_demo = xblock.problem:EqualityCheckerBlock',
-            'attempts_scoreboard_demo = xblock.problem:AttemptsScoreboardBlock',
-            'slider_demo = xblock.slider:Slider',
-            'view_counter_demo = xblock.view_counter:ViewCounter',
-        ]
-    }
+    ]
 )
