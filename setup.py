14ba74869448bc58838ffa476f9ebf8cede3b104
David Baumgold <david@davidbaumgold.com>
2014-04-22 10:03:15 -0400

    Flesh out setup.py
    Including trove classifier for Python 3 support

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(
     name='XBlock',
     version='0.4a1',
+    author='edX',
     description='XBlock Core Library',
     packages=[
         'xblock',
@@ -14,5 +15,14 @@ setup(
         'webob',
         'pytz',
         'python-dateutil',
+    ],
+    license='AGPL',
+    classifiers=[
+        "Topic :: Education",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
     ]
 )
