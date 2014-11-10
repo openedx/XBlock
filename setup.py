ba6d5c457880b1ef424d2fcc484d5616cba734e3
Calen Pennington <cale@edx.org>
2014-11-10 13:50:38 -0500

    Add a makefile for running tests/packaging

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import versioneer
 versioneer.VCS = 'git'
 versioneer.versionfile_source = 'xblock/_version.py'
 versioneer.versionfile_build = 'xblock/_version.py'
-versioneer.tag_prefix = 'xblock-' # tags are like 1.2.0
-versioneer.parentdir_prefix = 'XBlock-' # dirname like 'myproject-1.2.0'
+versioneer.tag_prefix = 'xblock-'  # tags are like 1.2.0
+versioneer.parentdir_prefix = 'XBlock-'  # dirname like 'myproject-1.2.0'
 
 setup(
     name='XBlock',
