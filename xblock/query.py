5bd56b546da191ffd72b6eb9ed847faf443a0cd6
Xiaolu Xiong <beardeer@gmail.com>
2015-08-13 15:01:34 -0400

    pep8 changes

--- xblock/query.py
+++ xblock/query.py
@@ -1,12 +1,13 @@
 """
     Define Query and Shared objects. Query supports ad-hoc queries via get and set operators.
-    Shared uses `bind` functions to access data from pre-defined entities.  
+    Shared uses `bind` functions to access data from pre-defined entities.
 """
 
 from xblock.exceptions import SharedFieldAccessDeniedError
 
+
 class Query(object):
-    """ 
+    """
     A query object for accessing shared field in XBlock with Queryable operators
 
     Parameters:
@@ -28,6 +29,7 @@ class Query(object):
     def __set__(self, xblock, val):
         raise AttributeError("can't set attribute")
 
+
 class Shared(object):
     """
     A shared object for accesing shared fields in XBlock with descriptor protocol
@@ -35,17 +37,17 @@ class Shared(object):
     Parameters:
 
         field_name (str): the name of the shared field
-        bind_attr_name (str): the name of XBlock instance property. 
-            This property is used to get query information.  
+        bind_attr_name (str): the name of XBlock instance property.
+            This property is used to get query information.
     """
     def __init__(self, field_name, bind_attr_name):
         self._file_name = field_name
         self._bind_attr_name = bind_attr_name
-    
+
     def __get__(self, xblock, xblock_class):
         if xblock is None:
             return self
-        
+
         self._queryable = Queryable(field_name=self._file_name)
         bind = getattr(xblock, self._bind_attr_name)
         # pylint: disable=protected-access
@@ -63,7 +65,7 @@ class Shared(object):
 class Queryable(object):
     """
     A Queryable object that contains operations for reading and writing to shared fields.
-    
+
     Parameters:
         field_name (str): the name of a shared field
 
@@ -77,7 +79,7 @@ class Queryable(object):
     def field_name(self):
         """
         Return the name of shared field that this queryable object is working on
-        
+
         Returns:
             str: a field name
         """
@@ -86,10 +88,10 @@ class Queryable(object):
     def _attach_current_block(self, block):
         """
         Attach the current XBlock instance to this queryable object
-        
+
         Args:
             block (XBlock): the current XBlock instance
-        
+
         Returns:
             None
         """
@@ -100,11 +102,11 @@ class Queryable(object):
         Attach this queryable instance to xblock. The queryable
         instance is used by platform to know this is a call to
         a shared field.
-        
+
         Args:
             xblock (XBlock): the target XBlock that is being queried
             field_name (str): the name of shared field
-        
+
         Returns:
             None
         """
@@ -113,11 +115,11 @@ class Queryable(object):
     def _detach_queryable_to_field(self, xblock, field_name):
         """
         Detach the queryable instance from a XBlock after queryies
-        
+
         Args:
             xblock (XBlock): the target XBlock that has being queried
             field_name (TYPE): the name of shared field
-        
+
         Returns:
             None
         """
@@ -125,14 +127,14 @@ class Queryable(object):
 
     def _get_target_block(self, current_block, user_id, usage_id):
         """
-        This method builds the target XBlock that is being queried 
+        This method builds the target XBlock that is being queried
         from user_id and usage_id
-        
+
         Args:
             current_block (XBlock): Description
             user_id (str): the id of user that is being queried
             usage_id (str): the usage id of XBlock that is being queried
-        
+
         Returns:
             XBlock: the target XBlock that is being queried
         """
@@ -144,16 +146,16 @@ class Queryable(object):
     def _check_remote_scope_premission(self, field_name, current_block, target_block):
         """
         This method checks the defined remote scope in shared field
-        with current xblock to determine if current xblock has the 
+        with current xblock to determine if current xblock has the
         permission to access the shared field in target xblock.
 
-        Rise SharedFieldAccessDeniedError if permission denied. 
-        
+        Rise SharedFieldAccessDeniedError if permission denied.
+
         Args:
             field_name (str): the name of shared field in target xblock
-            current_block (XBlock): the current XBlock instance 
+            current_block (XBlock): the current XBlock instance
             target_block (XBlock): the target XBlock instance
-        
+
         Returns:
             None
         """
@@ -163,9 +165,9 @@ class Queryable(object):
         target_scope_ids = target_block.scope_ids
 
         if shared:
-        # We are not allowing cross-course sharing due to:
-        # 1) Security concerns
-        # 2) Demonstrates fine-grained access control
+            # We are not allowing cross-course sharing due to:
+            # 1) Security concerns
+            # 2) Demonstrates fine-grained access control
             if current_scope_ids.def_id.course_key != target_scope_ids.def_id.course_key:
                 raise SharedFieldAccessDeniedError
         else:
@@ -174,7 +176,7 @@ class Queryable(object):
     def get(self, user_id=None, usage_id=None):
         """
         The get operator for Queryable class
-        
+
         Args:
             user_id (str): the id of user that is being queried
             usage_id (str, optional): the usage id of XBlock that is being queried
@@ -182,7 +184,7 @@ class Queryable(object):
         field_name = self._field_name
         current_block = self.current_block
         target_block = self._get_target_block(current_block, user_id, usage_id)
-     
+
         try:
             self._check_remote_scope_premission(field_name, current_block, target_block)
         except SharedFieldAccessDeniedError:
@@ -207,7 +209,7 @@ class Queryable(object):
     def set(self, value, user_id=None, usage_id=None):
         """
         The set operator for Queryable class
-        
+
         Args:
             value (any): the new value
             user_id (str): the id of user that is being queried
