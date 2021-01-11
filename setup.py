1096e349dc78eef45cdae38addc2f127d6fa05ea
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-01-11 13:44:24 +0500

    Fixing test failure.

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.8",
     ]
 )
