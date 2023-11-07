2305c9ba7c55fa7b502085689d62e0002f44b764
Feanil Patel <feanil@axim.org>
2023-11-07 09:59:35 -0500

    test: Don't depend on the xblock-sdk runtime.
    We don't want to depend on the xblock-sdk WorkbenchRuntime because it
    will cause a circular dependency between tests in xblock and the
    released version of the workbench which already depends on XBlock.
    
    Instead use the ToyRuntime available for testing in this repository and
    don't run the workbench during testing.

--- xblock/test/settings.py
+++ xblock/test/settings.py
@@ -109,7 +109,6 @@ INSTALLED_APPS = (
 
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'workbench'
 )
 
 # A sample logging configuration. The only tangible logging
