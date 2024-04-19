b97e37624708ed10e5c385c79c4b7a2237f890f9
connorhaugh <49422820+connorhaugh@users.noreply.github.com>
2024-04-19 09:33:48 -0400

    feat: readd fragment as passthrough to webfragement.fragment (#739)
    a previous deprecation of this pass-through causes wide-spread consequences.

---
+++ xblock/fragment.py
@@ -0,0 +1,25 @@
+"""
+Makes the Fragment class available through the old namespace location.
+"""
+import warnings
+
+import web_fragments.fragment
+
+
+class Fragment(web_fragments.fragment.Fragment):
+    """
+    A wrapper around web_fragments.fragment.Fragment that provides
+    backwards compatibility for the old location.
+    Deprecated.
+    """
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            'xblock.fragment is deprecated. Please use web_fragments.fragment instead',
+            DeprecationWarning,
+            stacklevel=2
+        )
+        super().__init__(*args, **kwargs)
+
+    # Provide older names for renamed methods
+    add_frag_resources = web_fragments.fragment.Fragment.add_fragment_resources
+    add_frags_resources = web_fragments.fragment.Fragment.add_resources
